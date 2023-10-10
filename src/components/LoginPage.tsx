43fce22217c333e70fb670f7a70a514d16969a0a
Jonata Silva <jonatasilva118@gmail.com>
2023-10-10 13:16:22 -0300

    feat: add login page integration with api

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,45 +1,38 @@
-import { FormEvent, useState } from 'react'
+import { FormEvent, useState } from "react";
 
 export function LoginPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = async (ev: FormEvent) => {
     ev.preventDefault();
-    
     try {
-      const response = await fetch('http://localhost:3333/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email, password })
-      });
-
-      console.log(response)
-      // if (response.ok) {
-      //   const { token } = await response.json();
-        
-      //   // Armazene o token de autenticação no armazenamento do navegador
-      //   localStorage.setItem('token', token);
-      // } else {
-      //   const error = await response.json();
-        
-      //   // Exiba uma mensagem de erro informando que o email ou a senha estão incorretos
-      //   console.log(error.message);
-      // }
-    } catch (error) {
-      console.error(error);
+      const response = await fetch(
+        "https://predium-big-village-back-end.vercel.app/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+      return data;
+    } catch (err) {
+      alert("Email ou senha");
+      console.error("Alguma coisa deu merda parceiro", err);
+      return [];
     }
   };
-
   return (
     <div className="hero min-h-2/4 bg-white/50 rounded-md shadow-md">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold">Login now!</h1>
           <p className="py-6">
-          Bem-vindo(a)! Faça login para acessar sua conta.
+            Bem-vindo(a)! Faça login para acessar sua conta.
           </p>
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-zinc-200">
@@ -50,9 +43,9 @@ export function LoginPage() {
               </label>
               <input
                 type="email"
-                id='email'
+                id="email"
                 value={email}
-                onChange={(ev)=> setEmail(ev.target.value)}
+                onChange={(ev) => setEmail(ev.target.value)}
                 placeholder="email"
                 className="input input-ghost"
                 required
@@ -64,21 +57,26 @@ export function LoginPage() {
               </label>
               <input
                 type="password"
-                id='password'
+                id="password"
                 value={password}
-                onChange={(ev)=> setPassword(ev.target.value)}
+                onChange={(ev) => setPassword(ev.target.value)}
                 placeholder="password"
                 className="input input-ghost"
                 required
               />
               <label className="label">
-                <a href="#" className="label-text-alt link link-hover text-black/60">
+                <a
+                  href="#"
+                  className="label-text-alt link link-hover text-black/60"
+                >
                   Forgot password?
                 </a>
               </label>
             </div>
             <div className="form-control mt-6">
-              <button type='submit' className="btn btn-accent">Login</button>
+              <button type="submit" className="btn btn-accent">
+                Login
+              </button>
             </div>
           </form>
         </div>
