ebb11ac6a2db215fced54ad3a79a76c94da96699
Jonata Silva <jonatasilva118@gmail.com>
2023-10-26 16:39:17 -0300

    refac: new resident form layout

--- src/components/Residents/ResidentContent.tsx
+++ src/components/Residents/ResidentContent.tsx
@@ -2,12 +2,12 @@ import { ResidentForm } from "./ResidentForm";
 
 export function ResidentContent() {
   return (
-    <div className="flex flex-col items-start w-3/4 rounded-lg px-2 pb-2 shadow-md overflow-auto h-5/6">
+    <div className="flex flex-col items-start w-full rounded-lg px-2 pb-2 shadow-md overflow-auto h-5/6">
       <div className="flex h-10 justify-center items-center py-1 self-stretch bg-zinc-100">
         <h1 className="font-bold text-black">Novo Morador</h1>
       </div>
       <div className="flex gap-2 flex-wrap justify-between p-3 items-start">
-        <div className="flex w-full flex-wrap md:w-2/4">
+        <div className="flex w-full justify-center gap-2 flex-wrap">
           <ResidentForm />
         </div>
         <div className="flex flex-wrap gap-2 flex-col max-w-xs">
